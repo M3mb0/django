8e86e5cfe96c5dc1df038d46a42963e464a82b54
Ungureanu Cristian Dumitru <108155938+M3mb0@users.noreply.github.com>
2022-12-06 12:03:06 +0200

    Update settings.py

--- yt_mp3/yt_mp3/settings.py
+++ yt_mp3/yt_mp3/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#@m-3l@6co-5t#2hugw2g372-^8t5$ukijdg7n%$@s-+4@*)jy'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
